07f1a0b1bba7ab0c1a1f325cf2487e5c6386eae0
Dmitriy <dmitriy7smirnov@gmail.com>
2020-02-03 21:09:37 +0300

    changed compare logic
    Signed-off-by: Dmitriy <dmitriy7smirnov@gmail.com>

--- SorterByLength.java
+++ SorterByLength.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 public class SorterByLength implements Comparator<String> {
     @Override
     public int compare(String str1, String str2) {
-        return 0;
+        if (str1.length() < str2.length()){
+            return 1;
+        }else if (str1.length() > str2.length()){
+            return -1;
+        }
+        return str1.toLowerCase().compareTo(str2.toLowerCase());
     }
 }
